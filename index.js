b348f2ed60fdac4768aaa6846f7bffe377baa3cb
Jisse Reitsma <jisse@yireo.com>
2019-12-13 10:44:31 +0100

    Code formatting

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { setupMultistoreRoutes } from "@vue-storefront/core/lib/multistore";
-import routes from "./router";
-import "@vue-storefront/core/lib/passive-listeners";
-import { RouterManager } from "@vue-storefront/core/lib/router-manager";
-import { themeEntry } from "../default";
+import { setupMultistoreRoutes } from '@vue-storefront/core/lib/multistore';
+import routes from './router';
+import '@vue-storefront/core/lib/passive-listeners';
+import { RouterManager } from '@vue-storefront/core/lib/router-manager';
+import { themeEntry } from '../default';
 
-const initTheme = function(app, router, store, config, ssrContext) {
+const initTheme = function (app, router, store, config, ssrContext) {
   setupMultistoreRoutes(config, router, routes);
   RouterManager.addRoutes(routes, router);
 };
